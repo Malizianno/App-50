5ca1537e3ae15c89bc7a6fb222b69d4e46f184c7
Malizianno <cristian.sterie@yahoo.com>
2017-08-16 21:44:07 +0300

    Configuration Page All Time

--- app/src/main/java/com/moonlighttm/app50/MainActivity.java
+++ app/src/main/java/com/moonlighttm/app50/MainActivity.java
@@ -1,5 +1,6 @@
 package com.moonlighttm.app50;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -23,15 +27,16 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // clicking on fab button
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                onButtonPressed(view, ConfigActivity.class);
             }
         });
 
+        // toggle bar
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -72,6 +77,14 @@ public class MainActivity extends AppCompatActivity
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Called when the settings button is pushed/pressed/tapped :)))
+     */
+    public void onButtonPressed(View view, Class cls) {
+        Intent intent = new Intent(this, cls);
+        startActivity(intent);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
